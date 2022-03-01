83e8aea1b5630c0e26abae44b0305d442593e17e
Isa <isamitre@gmail.com>
2022-03-01 12:46:39 -0500

    can get a list of logged in user's playlists

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,17 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-    const CLIENT_ID = "b39c9c2f4fa346a69e4cdbcafefd5185"
-    const REDIRECT_URI = "http://localhost:3000"
+    const CLIENT_ID = "b1973aa897914a7a8b045880ef919a81"
+    const REDIRECT_URI = "http://localhost:3000/";
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
 
     const [token, setToken] = useState("")
-    const [searchKey, setSearchKey] = useState("")
-    const [artists, setArtists] = useState([])
     const [playlists, setPlaylists] = useState([])
-
-    // const getToken = () => {
-    //     let urlParams = new URLSearchParams(window.location.hash.replace("#","?"));
-    //     let token = urlParams.get('access_token');
-    // }
 
     useEffect(() => {
         const hash = window.location.hash
         let token = window.localStorage.getItem("token")
-
-        // getToken()
-
 
         if (!token && hash) {
             token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
@@ -41,72 +31,47 @@ function App() {
         window.localStorage.removeItem("token")
     }
 
-    const searchArtists = async (e) => {
-        e.preventDefault()
-        const {data} = await axios.get("https://api.spotify.com/v1/search", {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            params: {
-                q: searchKey,
-                type: "artist"
-            }
-        })
-
-        setArtists(data.artists.items)
-    }
-
     const getPlaylists = async (e) => {
-        console.log("get playlists");
         e.preventDefault()
-        const {data} = await axios.get("https://api.spotify.com/v1/me/playlists", {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-        })
-        console.log(data);
+        const {data} = await axios
+            .get("https://api.spotify.com/v1/me/playlists", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            })
+            .catch((error) => {
+                console.log(error);
+            });
 
-        setPlaylists(data)
+        setPlaylists(data.items)
     }
 
-    const renderPlaylists = async (e) => {
-        return data.items.map((item) => (
-            <p>{item.name}</p>
-        ));
-    }
-
-    const renderArtists = () => {
-        return artists.map(artist => (
-            <div key={artist.id}>
-                {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-                {artist.name}
+    const renderPlaylists = () => {
+        return playlists.map(playlist => (
+            <div key={playlist.id}>
+                {playlist.images.length ? <img width={"100%"} src={playlist.images[0].url} alt=""/> : <div>No Image</div>}
+                {playlist.name}
             </div>
         ))
     }
 
     return (
-        <div className="App">
-            <header className="App-header">
+        <div id="App">
+            <div id="App-header">
                 <h1>Spotify React</h1>
                 {!token ?
                     <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
                         to Spotify</a>
-                    : <button onClick={logout}>Logout</button>}
-
+                    : <button onClick={logout}>Logout</button>
+                }
                 {token ?
-                    // <form onSubmit={searchArtists}>
-                    //     <input type="text" onChange={e => setSearchKey(e.target.value)}/>
-                    //     <button type={"submit"}>Search</button>
-                    // </form>
-                    <button onClick={getPlaylists}>get playlists</button>
-
+                    <div>
+                        <button onClick={getPlaylists}>get playlists</button>
+                    </div>
                     : <h2>Please login</h2>
                 }
-
-                {/* {renderArtists()} */}
-                { renderPlaylists}
-
-            </header>
+                { renderPlaylists() }
+            </div>
         </div>
     );
 }
