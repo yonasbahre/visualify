7ab8030ea14c18faebac854cde4b5d5b96678f43
Isa <isamitre@gmail.com>
2022-03-13 13:48:02 -0400

    added comments

--- client/src/components/Recommendations.js
+++ client/src/components/Recommendations.js
@@ -4,10 +4,15 @@ import axios from 'axios';
 function Recommendations({token, features, songIDs}) {
     const [recommendations, setRecommendations] = useState([])
 
+    // up to how many unique songs will returned at the end
     const totalRecLimit = 20;
 
+    // calculates how many songs should be gotten per request is needed to get 50 songs
+    // based on how many given song IDs there are
     const limitPerRequest = Math.ceil(50 / Math.ceil(songIDs.length/3));
 
+    // create getRequest
+    // rn it has genre and artist hardcoded in
     function getRequest(currentSongIDs) {
         let getReq = "https://api.spotify.com/v1/recommendations?"
             + "limit=" + limitPerRequest 
@@ -31,11 +36,12 @@ function Recommendations({token, features, songIDs}) {
 
     const getRecommendations = async (e) => {
         let tempRecs = []
+        // loops through 3 songs at a time 
         for (let i = 0; i < songIDs.length; i+=3) {
             // use three songs per recommendation
             const currentSongIDs = songIDs.slice(i, i + 3);
             
-            // get recommended song
+            // get recommended songs
             const {data} = await axios
                 .get(getRequest(currentSongIDs), {
                     headers: {
@@ -45,14 +51,21 @@ function Recommendations({token, features, songIDs}) {
                 .catch((error) => {
                     console.log(error);
                 });
+            
+            // add songs from request to array
             tempRecs = tempRecs.concat(data.tracks);
         }
+
+        // remove any duplicate songs
+        // https://stackoverflow.com/questions/2218999/how-to-remove-all-duplicates-from-an-array-of-objects
         let uniqueRecs = tempRecs.filter((value, index) => {
             const _value = JSON.stringify(value);
             return index === tempRecs.findIndex(obj => {
               return JSON.stringify(obj) === _value;
             });
         });
+
+        // limits length of array to totalRecLimit
         if (uniqueRecs.length > totalRecLimit) {
             uniqueRecs = uniqueRecs.slice(0, totalRecLimit);
         }
@@ -60,6 +73,7 @@ function Recommendations({token, features, songIDs}) {
         setRecommendations(uniqueRecs);        
     }
 
+    // renders recs as a bullet point list
     const renderRecs = () => {
         return recommendations.map(rec => (
             <li key={rec.id}>{rec.name}</li>
